56ff3e222f957d521e6034347d34b3ad6c766edf
Szymon <szymon@konto.pl>
2023-09-04 00:06:49 +0200

    solved

--- 03_04/selection_sort_stub.py
+++ 03_04/selection_sort_stub.py
@@ -1,8 +1,22 @@
+def find_min(l):
+    min_index=0
+    for idx, val in enumerate(l):
+        if l[idx] < l[min_index]:
+            min_index = idx
+    return min_index
+
 def selection_sort(xs):
-    pass
+    xs_size = len(xs)
+    for idx in range(xs_size-1):
+        min_idx = find_min(xs[idx+1:])+idx
+        if xs[idx] > xs[min_idx+1]:
+            xs[idx], xs[min_idx+1] = xs[min_idx+1], xs[idx]
+    return xs
+
 
 
 xs = [3, 2, 1, 5, 4]
+
 print(xs)
 selection_sort(xs)
 print(xs)
